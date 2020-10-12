3eadd555a678b2bb6b6c52a674a50ec0e8f84912
imimran <engr.aih@gmail.com>
2020-10-12 17:21:50 +0600

    chenge port

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,13 @@
 const User = require("../models/user");
 const { success, fail, validation } = require("../utils/helper");
 const bcrypt = require("bcrypt");
+const { use } = require("../routes/user/user");
 
 exports.getAllUser = async (req, res) => {
   try {
     const users = await User.findAll();
     res.status(200).json(success("OK", { data: users }, res.statusCode));
- 
+
     return;
   } catch (error) {
     console.log(error);
@@ -39,19 +40,29 @@ exports.addUser = async (req, res) => {
     const password = req.body.password;
 
     if (!username || !email || !password) {
-      return res.status(422).json(validation( "Please input all field", res.statusCode));
+      return res
+        .status(422)
+        .json(validation("Please input all field", res.statusCode));
     }
 
-    let preUser = await User.findOne({ where: { email: req.body.email }});
-    if (preUser) return res.status(400).json(validation("User already registered.", res.statusCode));
+    let preUser = await User.findOne({ where: { email: req.body.email } });
+    if (preUser)
+      return res
+        .status(400)
+        .json(validation("User already registered.", res.statusCode));
 
     const user = await User.create({
       username: username,
       email: email,
       password: password,
     });
+
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+     
+    
+
     res.status(200).json(success("OK", { data: user }, res.statusCode));
   } catch (error) {
     console.log(error);

270c3a60c7487f3bfdba2385144ccb4a33b8768c
imimran <engr.aih@gmail.com>
2020-11-08 02:51:09 +0600

    install nodemon

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('employee', 'root', '', {
+const sequelize = new Sequelize('employee', 'root', '@capTain100', {
     dialect: 'mysql',
     host: 'localhost'
+    
 })
 
 module.exports = sequelize

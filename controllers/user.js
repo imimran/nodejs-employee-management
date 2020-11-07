67331b2ee095cfa5832d49f245218e28cfe442e6
imimran <engr.aih@gmail.com>
2020-11-07 14:51:28 +0600

    fix hash password

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,14 @@
-const User = require("../models/user");
-const { success, fail, validation } = require("../utils/helper");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { jwtKey } = require("../utils/key");
-const _ = require('lodash')
+const User = require('../models/user');
+const { success, fail, validation } = require('../utils/helper');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { jwtKey } = require('../utils/key');
+const _ = require('lodash');
 
 exports.getAllUser = async (req, res) => {
   try {
     const users = await User.findAll();
-    res.status(200).json(success("OK", { data: users }, res.statusCode));
+    res.status(200).json(success('OK', { data: users }, res.statusCode));
 
     return;
   } catch (error) {
@@ -21,11 +21,11 @@ exports.getAllUser = async (req, res) => {
 exports.getUser = async (req, res) => {
   try {
     if (!req.params.id) {
-      throw "No Match ID";
+      throw 'No Match ID';
     }
 
     const user = await User.findByPk(req.params.id);
-    res.status(200).json(success("OK", { data: user }, res.statusCode));
+    res.status(200).json(success('OK', { data: user }, res.statusCode));
     //.send(user);
     return;
   } catch (error) {
@@ -42,32 +42,34 @@ exports.addUser = async (req, res) => {
     const password = req.body.password;
 
     if (!username || !email || !password) {
-      return res.status(501).json(validation("Please input all field"));
-    } 
-
+      return res.status(501).json(validation('Please input all field'));
+    }
 
     let preUser = await User.findOne({ where: { email: req.body.email } });
     if (preUser)
       return res
         .status(501)
-        .json(validation({ email: "User already registered." }));
+        .json(validation({ email: 'User already registered.' }));
 
-    const picked = _.pick(req.body, ['username', 'email', 'password'])    
+    const picked = _.pick(req.body, ['username', 'email', 'password']);
 
     const user = await User.create(picked);
 
-    // const salt = await bcrypt.genSalt(10);
-    // user.password = await bcrypt.hash(user.password, salt);
-    // await user.save();
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
 
-    const token = jwt.sign({ id: user.id, email: user.email }, jwtKey);
+    const token = jwt.sign({ id: user.id, email: user.email }, jwtKey, {
+      expiresIn: 3600,
+    });
     res
       .status(200)
-      .json(success("Your Token! ", { data: token }, res.statusCode));
-    // res.status(200).json(success("OK", { data: user }, res.statusCode));
+      .json(success('Your Token! ', { data: token }, res.statusCode));
+    // res.status(200).json(success('OK', { data: user }, res.statusCode));
 
     return;
   } catch (error) {
+    console.log(error);
     res.status(500).json(fail(error, res.statusCode));
     return;
   }
--- routes/user/auth.js
+++ routes/user/auth.js
@@ -1,28 +1,32 @@
-const express = require("express");
-const User = require("../../models/user");
+const express = require('express');
+const User = require('../../models/user');
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const { jwtKey } = require("../../utils/key");
-const bcrypt = require("bcrypt");
-const { validation, success } = require("../../utils/helper");
+const jwt = require('jsonwebtoken');
+const { jwtKey } = require('../../utils/key');
+const bcrypt = require('bcrypt');
+const { validation, success } = require('../../utils/helper');
 
-router.get("/", (req, res) => {
-  res.send("Hello Empolyee");
+router.get('/', (req, res) => {
+  res.send('Hello Empolyee');
 });
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(501).json(validation("please add email or password"));
+    return res.status(501).json(validation('please add email or password'));
   }
 
   let user = await User.findOne({ where: { email: req.body.email } });
-  if (!user) return res.status(400).json(validation("User Not registered."));
+  if (!user) return res.status(501).json(validation('User Not registered.'));
 
-  bcrypt.compare(req.body.password, user.password);
-  if (req.body.password !== user.password)
-    return res.status(400).json(validation("Invalid email or password "));
+  // bcrypt.compare(req.body.password, user.password);
+  // if (req.body.password !== user.password)
+  //   return res.status(400).json(validation("Invalid email or password "));
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(501).json(validation('Invalid email or password '));
 
   const token = jwt.sign(
     {
@@ -38,7 +42,7 @@ router.post("/login", async (req, res) => {
     .status(200)
     .json(
       success(
-        "Your Token! ",
+        'Your Token! ',
         { data: token, id: user.id, email: user.email },
         res.statusCode
       )

3fcb54302b996350330390a656ea868bc5864236
Al Imran Hossain <engr.aih@gmail.com>
2021-08-11 15:47:10 +0600

    Update status

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -6,14 +6,14 @@ const User = require('../models/user')
  async function auth( req, res, next){
      const token = req.header('auth-token');
      if(!token)
-     return res.status(501).json(validation("Authorization Failed.No token Provided"))
+     return res.status(401).json(validation("Authorization Failed.No token Provided"))
 
      try {
          const decoded = jwt.verify(token, jwtKey)
          req.user = await User.findByPk(decoded.id)
          next()         
      } catch (error) {
-         res.status(500).json(validation("Authorization Failed. Invald token"))
+         res.status(401).json(validation("Authorization Failed. Invald token"))
      }
  }
 

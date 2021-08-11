7bedeedbcbce8b0a5b41b3b7129c9a5a41220907
Al Imran Hossain <engr.aih@gmail.com>
2021-08-11 15:47:48 +0600

    Update isRoll.js

--- middlewares/isRoll.js
+++ middlewares/isRoll.js
@@ -3,19 +3,19 @@ const {validation} = require('../utils/helper')
 
 exports.isAdmin = async(req, res, next) => {
    if (!req.user.isAdmin) 
-    return res.status(501).json(validation("You are not allowed to access."));
+    return res.status(401).json(validation("You are not allowed to access."));
     next();
 }
 
 exports.isEmployee = async (req, res, next) => {
   if (!req.user.isEmployee)
-    return res.status(501).json(validation("You are not allowed to access."));
+    return res.status(401).json(validation("You are not allowed to access."));
   next();
 };
 
 exports.isOrganizer = async (req, res, next) => {
   if (!req.user.isOrganizer)
-    return res.status(501).json(validation("You are not allowed to access."));
+    return res.status(401).json(validation("You are not allowed to access."));
   next();
 };
 
